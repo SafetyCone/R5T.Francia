48f44554f58d5defbd9dc0dfa42b854f2d7f53a5
MinexAutomation <minex.automation@gmail.com>
2019-11-07 17:31:36 -0800

    Added ImageIdentity TypedGuid type.

--- source/R5T.Francia/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Francia
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Francia/Code/Classes/ImageIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Francia
+{
+    public class ImageIdentity : TypedGuid
+    {
+        public ImageIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public ImageIdentity()
+            : this(Guid.NewGuid())
+        {
+        }
+    }
+}
