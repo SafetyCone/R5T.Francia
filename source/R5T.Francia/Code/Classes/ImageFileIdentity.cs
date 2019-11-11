ae72e38408b9ee2b7c742bca3b4069b2a59b94e1
MinexAutomation <minex.automation@gmail.com>
2019-11-11 00:36:25 -0800

    Added static New() method.

--- source/R5T.Francia/Code/Classes/ImageFileIdentity.cs
+++ source/R5T.Francia/Code/Classes/ImageFileIdentity.cs
@@ -7,6 +7,17 @@ namespace R5T.Francia
 {
     public class ImageFileIdentity : TypedGuid
     {
+        #region Static
+
+        public static ImageFileIdentity New()
+        {
+            var output = new ImageFileIdentity();
+            return output;
+        }
+
+        #endregion
+
+
         public ImageFileIdentity(Guid value)
             : base(value)
         {
